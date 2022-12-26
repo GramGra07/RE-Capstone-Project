d6b8f28dd2f5a73f5ca06a0ee60fb7941fbeac17
Graden <101433010+GramGra07@users.noreply.github.com>
2022-12-26 16:34:12 -0700

    Update RoughSample.ino

--- Code/RoughSample.ino
+++ Code/RoughSample.ino
@@ -1,34 +1,154 @@
+
+//lcd
 #include <LiquidCrystal.h>
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-char message[]="hello worldyybyy opp";
+const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
+LiquidCrystal lcd(rs, en, d4,d5, d6, d7);
+String message="Plz tap center button";
 int y = 0;
-int x = sizeof(message);
+int x = message.length()-1;
 String m ="";
+String actions="";
+//button
+const int acceptBPin = 7, fBpin = 6, bBpin = 5, rBpin = 4, lBpin = 3;// using 10-6 as inputs
+int aBS = 0, fBS =0, bBS=0, rBS = 0, lBS =0;//button states
+//motor, only room for 3, middle wheel needs to be omni
+const int motorBR = 3, motorBL= 2, motorTurn = 1;
+//other
+//modes: selecting direction, selecting length, accept?
+boolean isStarted=true;
+boolean directionS = true;
+boolean lengthS = false;
+boolean accepted = false;
+boolean runOVR = false;
+boolean hasRun = false;
+//Serial.println(message.length());
 void setup() {
+  pinMode(acceptBPin,INPUT);
+  pinMode(fBpin,INPUT);
+  pinMode(bBpin,INPUT);
+  pinMode(rBpin,INPUT);
+  pinMode(lBpin,INPUT);
   Serial.begin(9600);
   lcd.begin(16, 2);
-  print();
+  print(15);//starting message
 }
 void loop() {
+  setupButton();
   lcd.cursor();
-  lcd.setCursor(x, y);
-}
-void print(){
-  if (sizeof(message)>=16){
-    x=0+sizeof(message)-16;
-    y=1;
-    makeMessage(0,16);
-    Serial.println(m);
-    delay(5000);
-    makeMessage(16,sizeof(message));
-    Serial.println(m);
+  if (aBS==HIGH){
+    if (not isStarted){
+      isStarted=true;
+    }
+    if (not accepted){
+      accepted=true;
+    }
   }
-  lcd.print(message);
+  if (isStarted){
+    lcd.setCursor(x, y);
+    if (directionS){
+      if (not hasRun){
+        resetLCD();
+        message="Select direction:";
+        print(15);
+        handleButton();
+        hasRun=true;
+      }
+    }
+  }
+  Serial.println(actions);
 }
-void makeMessage(int start, int end){
+void resetLCD(){
+  x=0;
+  y=0;
+  lcd.setCursor(x,y);
+  lcd.clear();
+  message="";
+}
+void setupButton(){
+  aBS = digitalRead(acceptBPin);
+  fBS = digitalRead(fBpin);
+  bBS = digitalRead(bBpin);
+  rBS = digitalRead(rBpin);
+  lBS = digitalRead(lBpin);
+}
+void checkAccepted(){
+  if (aBS==HIGH and not accepted){
+    accepted=true;
+  }
+}
+void handleButton(){
+  fBS=HIGH;
+  if (fBS==HIGH){
+    //fwd
+    actions+="F,";
+    accepted=false;
+    if(not accepted){
+      resetLCD();
+      message="Press center to accept: F";
+      aBS=HIGH;
+      print(16);
+      checkAccepted();
+    }
+  }
+  if (bBS==HIGH){
+    //bwd
+    actions+="B,";
+    message+="B";
+  }
+  if (rBS==HIGH){
+    //right
+    actions+="R,";
+    message+="R";
+  }
+  if (lBS==HIGH){
+    //leftd
+    actions+="L,";
+    message+="L";
+  }
+}
+void print(int start){//start will usually be 16 but changed to make not cut off words
+  if (message.length()>=16){
+    makeMessage(0,start);
+    lcd.print(m);
+    x=message.length()-start;
+    y=1;
+    lcd.setCursor(0,1);
+    makeMessage(start,message.length());
+    lcd.print(m);
+  }else{
+    lcd.print(message);
+  }
+}
+void makeMessage(int s, int e){
   m ="";
-  for (int i = start; i<end;i++){
+  for (int i = s; i<e;i++){
     m+=message[i];
+  }
+}
+void runMotors(int d, int t){//distance and time
+  //   1
+  //2    3
+  //   4
+  if (d==1){
+    //go forward
+    delay(t*1000);
+    //stop
+  }
+  if (d==2){
+    //turn -90
+    //go forward
+    delay(t*1000);
+    //stop
+  }
+  if (d==3){
+    //turn 90
+    //go forward
+    delay(t*1000);
+    //stop
+  }
+  if (d==4){
+    //go backward
+    delay(t*1000);
+    //stop
   }
 }
