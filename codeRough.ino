8075f2135f7654621cdf445a2bd5dd2dd8ac6cc0
Graden <101433010+GramGra07@users.noreply.github.com>
2022-12-28 17:02:54 -0700

    Update codeRough.ino

--- codeRough.ino
+++ codeRough.ino
@@ -1,20 +1,19 @@
 //lcd
 #include <LiquidCrystal.h>
 const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
-LiquidCrystal lcd(rs, en, d4,d5, d6, d7);
-String message="Plz tap center button";
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+String message = "Plz tap center button";
 int y = 0;
-int x = message.length()-1;
+int x = message.length() - 1;
 int t = 0;
-String m ="";
-String actions="";
+String m = "";
+String actions = "";
 //button
-const int acceptBPin = 7, fBpin = 6, bBpin = 5, rBpin = 4, lBpin = 3;// using 10-6 as inputs
-int aBS = 0, fBS =0, bBS=0, rBS = 0, lBS =0;//button states
-//motor, only room for 3, middle wheel needs to be omni
-const int motorBR = 2, motorBL= 1, motorTurn = 0;
+const int acceptBPin = 7, fBpin = 6, rBpin = 5, lBpin = 4;// using 7-4 as inputs
+int aBS = 0, fBS = 0, bBS = 0, rBS = 0, lBS = 0; //button states
+const int motorBR = 3, motorBL = 2, motorFR = 1, motorFL = 0;
 //other
-boolean isStarted=false;
+boolean isStarted = false;
 boolean directionS = false;
 boolean lengthS = false;
 boolean accepted = false;
@@ -22,11 +21,10 @@ boolean runOVR = false;
 boolean hasRun = false;
 //Serial.println(message.length());
 void setup() {
-  pinMode(acceptBPin,INPUT);
-  pinMode(fBpin,INPUT);
-  pinMode(bBpin,INPUT);
-  pinMode(rBpin,INPUT);
-  pinMode(lBpin,INPUT);
+  pinMode(acceptBPin, INPUT);
+  pinMode(fBpin, INPUT);
+  pinMode(rBpin, INPUT);
+  pinMode(lBpin, INPUT);
   Serial.begin(9600);
   lcd.begin(16, 2);
   print(15);//starting message
@@ -34,157 +32,165 @@ void setup() {
 void loop() {
   setupButton();
   lcd.cursor();
-  if (not isStarted){
-    if (aBS==HIGH){
-      isStarted=true;
-      directionS=true;
+  if (not isStarted) {
+    if (aBS == HIGH) {
+      isStarted = true;
+      directionS = true;
     }
   }
-  while (isStarted){
-    if (directionS){
-      lengthS=false;
-      if (not hasRun){
-        resetLCD();
-        message="Select direction:";
-        print(15);
-        handleButton();
-      }
-    }
-    if (lengthS){
-      directionS=false;
-      if (not hasRun){
-        resetLCD();
-        message="Select time: ";
-        print(15);
-        handleButton();
-      }
-    }
-    delay(1000);
+  while (isStarted) {
+    doSelections(directionS, lengthS);
   }
   //Serial.println(actions);
 }
-void resetLCD(){
-  x=0;
-  y=0;
-  lcd.setCursor(x,y);
+void resetLCD() {
+  x = 0;
+  y = 0;
+  lcd.setCursor(x, y);
   lcd.clear();
-  message="";
+  message = "";
 }
-void setupButton(){
+void setupButton() {
   aBS = digitalRead(acceptBPin);
   fBS = digitalRead(fBpin);
-  bBS = digitalRead(bBpin);
   rBS = digitalRead(rBpin);
   lBS = digitalRead(lBpin);
 }
-void handleButton(){
-  //
-  //!none of this will work, need to get into mode then change stuff with new buttons
-  //
-  fBS=HIGH;
-  if (fBS==HIGH){
-    //fwd
-    if (directionS){
-      actions+="F,";
-      message+="F";
-      lcd.clear();
-      print(15);
-      setupButton();
-      if (aBS==HIGH){
-        Serial.println("H");
-        lengthS=true;
-        hasRun=false;
-        directionS=false;
-      }
-    }
-    if (lengthS){
-      setupButton();
-      t+=1;
-      message+=t;
-      lcd.clear();
-      lcd.print(message);
-      delay(500);
-      if (aBS==HIGH){
-        directionS=true;
-        hasRun=false;
-        lengthS=false;
-      }
-    }
-  }
-  if (bBS==HIGH){
-    //bwd
-    if (directionS){
-      actions+="B,";
-      accepted=false;
-      directionS=false;
-      lengthS=true;
-      resetLCD();
-    }
-  }
-  if (rBS==HIGH){
-    //right
-    if (directionS){
-      actions+="R,";
-      accepted=false;
-      directionS=false;
-      lengthS=true;
-      resetLCD();
-    }
-  }
-  if (lBS==HIGH){
-    //left
-    if (directionS){
-      actions+="L,";
-      accepted=false;
-      directionS=false;
-      lengthS=true;
-      resetLCD();
+boolean isAccepted() {
+  int a = digitalRead(acceptBPin);
+  while (a != HIGH) {
+    //not pressed
+    a = digitalRead(acceptBPin);
+    if (a == HIGH) {
+      return true;
+      break;
     }
   }
 }
-void print(int start){//start will usually be 16 but changed to make not cut off words
-  if (message.length()>=16){
-    makeMessage(0,start);
+void switchToL() {
+  lengthS = true;
+  directionS = false;
+}
+void switchToD() {
+  directionS = true;
+  lengthS = false;
+}
+void reBind() {
+  int a = digitalRead(acceptBPin);
+  int f = digitalRead(fBpin);
+  int r = digitalRead(rBpin);
+  int l = digitalRead(lBpin);
+}
+void doSelections(boolean d, boolean t) {
+  hasRun = false;
+  if (d) {
+    resetLCD();
+    message = "Select direction:  "; //last = [18]
+    print(15);
+    int a = digitalRead(acceptBPin);
+    int f = digitalRead(fBpin);
+    int r = digitalRead(rBpin);
+    int l = digitalRead(lBpin);
+    while (!isAccepted and not hasRun) {
+      if (f == HIGH) {
+        actions += "F";
+        message[17] = "F";
+        lcd.clear();
+        print(15);
+        if (isAccepted) {
+          switchToL();
+          hasRun = true;
+          break;
+        }
+      }
+      if (r == HIGH) {
+        actions += "R";
+        message += " R";
+        lcd.clear();
+        print(15);
+        if (isAccepted) {
+          switchToL();
+          hasRun = true;
+          break;
+        }
+      }
+      if (l == HIGH) {
+        actions += "L";
+        message += " L";
+        lcd.clear();
+        print(15);
+        if (isAccepted) {
+          switchToL();
+          hasRun = true;
+          break;
+        }
+      }
+    }
+  }
+  if (t) {
+    t = 0;
+    int a = digitalRead(acceptBPin);
+    int f = digitalRead(fBpin);
+    int r = digitalRead(rBpin);
+    int l = digitalRead(lBpin);
+    resetLCD();
+    message = "Select time (r = +, l = -): ";
+    print(15);
+    while (!isAccepted) {
+      if (r) {
+        t += 1;
+      }
+      if (l) {
+        if (t > 1) {
+          t -= 1;
+        }
+      }
+      message += t;
+      lcd.print(message);
+      if (isAccepted) {
+        switchToD();
+        break;
+      }
+    }
+  }
+}
+void print(int start) { //start will usually be 16 but changed to make not cut off words
+  if (message.length() >= 16) {
+    makeMessage(0, start);
     lcd.print(m);
-    x=message.length()-start;
-    y=1;
-    lcd.setCursor(0,1);
-    makeMessage(start,message.length());
+    x = message.length() - start;
+    y = 1;
+    lcd.setCursor(0, 1);
+    makeMessage(start, message.length());
     lcd.print(m);
-  }else{
+  } else {
     lcd.print(message);
   }
 }
-void makeMessage(int s, int e){
-  m ="";
-  for (int i = s; i<e;i++){
-    m+=message[i];
+void makeMessage(int s, int e) {
+  m = "";
+  for (int i = s; i < e; i++) {
+    m += message[i];
   }
 }
-void runMotors(int d, int t){//distance and time
+void runMotors(int d, int t) { //distance and time
   //   1
   //2    3
-  //   4
-  if (d==1){
+  if (d == 1) {
     //go forward
-    delay(t*1000);
+    delay(t * 1000);
     //stop
   }
-  if (d==2){
+  if (d == 2) {
     //turn -90
     //go forward
-    delay(t*1000);
+    delay(t * 1000);
     //stop
   }
-  if (d==3){
+  if (d == 3) {
     //turn 90
     //go forward
-    delay(t*1000);
-    //stop
-  }
-  if (d==4){
-    //go backward
-    delay(t*1000);
+    delay(t * 1000);
     //stop
   }
 }
