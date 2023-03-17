105ffa9349129117d890c31f3e53155650ecc5a7
Graden <101433010+GramGra07@users.noreply.github.com>
2023-03-17 09:32:35 -0600

    Update encoder.ino

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -7,7 +7,8 @@ int rCPose =0;
 boolean track = true;
 boolean lEHigh = false;
 boolean rEHigh = false;
-boolean isHigh = false;
+boolean isHighR = false;
+boolean isHighL = false;
 boolean hasRun = false;
 boolean finished = false;
 bool lRunF = true;
@@ -15,7 +16,7 @@ bool rRunF = true;
 int countsPerEncoder = 10;
 int wheelDiameter = 7;
 //float countsPerCM = (countsPerEncoder/(wheelDiameter*3.14159265358));
-double countsPerCM = 0.45;
+double countsPerCM = 0.09;//0.45
 int speed = 1 * 255;
 int enA = 27;
 int in1 = 26;
@@ -25,7 +26,7 @@ int enB = 23;
 int in3 = 24;
 int in4 = 22;
 
-int minimumDist = 5;
+int minimumDist = 10;
 int trigPin = 39;
 int echoPin = 38;
 // defines variables
@@ -51,7 +52,7 @@ void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  runToPosition(8,8);
+  runToPosition(30,30);
 }
 int getDistance(){
   digitalWrite(trigPin, LOW);
@@ -83,8 +84,6 @@ void runToPosition(double r,double l){
     l = round(l);
   }
   while (rCPose < r or lCPose<l and not finished){
-    Serial.println("l",lCPose);
-    Serial.println("r",rCPose);
     rCPose = int(rCPose);
     lCPose = int(lCPose);
     analogWrite(enA, speed);
@@ -104,19 +103,19 @@ void runToPosition(double r,double l){
       digitalWrite(in4, HIGH);
     }
     //run forward
-    if (digitalRead(lEpin)==0 and !isHigh){
-      isHigh = true;
+    if (digitalRead(lEpin)==0 and !isHighL){
+      isHighL = true;
       lCPose+=1;
     }
     if (digitalRead(lEpin)==1){
-      isHigh = false;
+      isHighL = false;
     }
-    if (digitalRead(rEpin)==0 and !isHigh){
-      isHigh = true;
+    if (digitalRead(rEpin)==0 and !isHighR){
+      isHighR = true;
       rCPose+=1;
     }
     if (digitalRead(rEpin)==1){
-      isHigh = false;
+      isHighR = false;
     }
     if (lCPose >= l){
       digitalWrite(in1, LOW);
@@ -130,10 +129,11 @@ void runToPosition(double r,double l){
       finished = true;
       break;
     }
-    //if (getDistance()<minimumDist){
-    //  stopMotors();
-    //  break;
-    //}
+    Serial.println(getDistance());
+    if (getDistance()<=minimumDist){
+      stopMotors();
+      break;
+    }
     
   }
 }
