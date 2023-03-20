29c1f646029a0f34923a688efef4e0a790057b09
Graden <101433010+GramGra07@users.noreply.github.com>
2023-03-20 09:50:23 -0600

    Update encoder.ino

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,10 +1,9 @@
 int lEpin = 48;
 int rEpin = 46;
 
-int lCPose =0;
+int lCPose =0;//left pose
 int i =0;
-int rCPose =0;
-boolean track = true;
+int rCPose =0;//right pose
 boolean lEHigh = false;
 boolean rEHigh = false;
 boolean isHighR = false;
@@ -15,9 +14,8 @@ bool lRunF = true;
 bool rRunF = true;
 int countsPerEncoder = 10;
 int wheelDiameter = 7;
-//float countsPerCM = (countsPerEncoder/(wheelDiameter*3.14159265358));
-double countsPerCM = 0.09;//0.45
-int speed = 1 * 255;
+double countsPerCM = 0.09;//0.45 // converts cm to counts
+int speed = 1 * 255; // sets speed
 int enA = 27;
 int in1 = 26;
 int in2 = 25;
@@ -55,7 +53,7 @@ void loop() {
   turnRight();
   turnLeft();
 }
-int getDistance(){
+int getDistance(){//will return distance
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -68,10 +66,10 @@ int getDistance(){
   return distance;
 }
 void turnLeft(){
-  runToPosition(22,-10);
+  runToPosition(22,-10);//calculated to turn left
 }
 void turnRight(){
-  runToPosition(-10,27);
+  runToPosition(-10,27);//calculated to turn right
 }
 void runToPosition(double r,double l){
   if (not hasRun){
@@ -93,50 +91,57 @@ void runToPosition(double r,double l){
   while ((rCPose < r or lCPose<l) and not finished){
     rCPose = int(rCPose);
     lCPose = int(lCPose);
-    analogWrite(enA, speed);
+    analogWrite(enA, speed);//set it to speed
     analogWrite(enB, speed);
-    if (lRunF){
+    if (lRunF){//if running forward
       digitalWrite(in1, HIGH);
       digitalWrite(in2, LOW);
-    }else{
+    }else{//running backward
       digitalWrite(in1, LOW);
       digitalWrite(in2, HIGH);
     }
-    if (rRunF){
+    if (rRunF){//running forward
       digitalWrite(in3, HIGH);
       digitalWrite(in4, LOW);
-    }else{
+    }else{//backwards
       digitalWrite(in3, LOW);
       digitalWrite(in4, HIGH);
     }
-    //run forward
+    //is already counted
     if (digitalRead(lEpin)==0 and !isHighL){
       isHighL = true;
       lCPose+=1;
     }
+    //was counted, be ready to count again
     if (digitalRead(lEpin)==1){
       isHighL = false;
     }
+    //check if its counted
     if (digitalRead(rEpin)==0 and !isHighR){
       isHighR = true;
       rCPose+=1;
     }
+    // was counted as a one
     if (digitalRead(rEpin)==1){
       isHighR = false;
     }
+    //if it has gone to far, stop
     if (lCPose >= l){
       digitalWrite(in1, LOW);
       digitalWrite(in2, LOW);
     }
+    //gone to far, stop
     if (rCPose >= r){
       digitalWrite(in3, LOW);
       digitalWrite(in4, LOW);
     }
+    //both to position, stop and reset
     if (lCPose>=l and rCPose>=r){
       finished = true;
       resetEncoders();
       break;
     }
+    //stop for distance
     //if (getDistance()<=(minimumDist/10)){
     //  stopMotors();
     //  finished = true;
